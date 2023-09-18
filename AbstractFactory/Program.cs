cd442e820bb2ba389de046999eddaabf61da2907
Gogs <gogs@fake.local>
2023-09-18 17:30:09 +0200

    Factory1

---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,50 @@
+﻿//
+abstract class AbstractFactory
+{
+    public abstract AbstractProductA CreateProductA();
+    public abstract AbstractProductB CreateProductB();
+}
+class ConcreteFactory1 : AbstractFactory
+{
+    public override AbstractProductA CreateProductA()
+    {
+        return new ProductA1();
+    }
+
+    public override AbstractProductB CreateProductB()
+    {
+        return new ProductB1();
+    }
+}
+
+class ConcreteFactory2 : AbstractFactory
+{
+    public override AbstractProductA CreateProductA()
+    {
+        return new ProductA2();
+    }
+
+    public override AbstractProductB CreateProductB()
+    {
+        return new ProductB2();
+    }
+}
+abstract class AbstractProductA { }
+abstract class AbstractProductB { }
+
+class ProductA1:AbstractProductA {}
+class ProductA2 : AbstractProductA {}
+class ProductB1 : AbstractProductB { }
+class ProductB2 : AbstractProductB { }
+
+class Client
+{
+    private AbstractProductA abstractProductA;
+    private AbstractProductB abstractProductB;
+
+    public Client(AbstractFactory factory)
+    {
+        this.abstractProductA = factory.CreateProductA();
+        this.abstractProductB = factory.CreateProductB();
+    }
+}
