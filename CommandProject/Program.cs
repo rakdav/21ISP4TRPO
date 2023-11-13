94b910801477573bf6e35793379ecf7cb42019f8
Gogs <gogs@fake.local>
2023-11-13 15:06:19 +0200

    Iterator

---
+++ CommandProject/Program.cs
@@ -0,0 +1,52 @@
+﻿abstract class Command
+{
+    public abstract void Execute();
+    public abstract void Undo();
+}
+class ConcreteCommand : Command
+{
+    Receiver receiver;
+    public ConcreteCommand(Receiver receiver)
+    {
+        this.receiver = receiver;
+    }
+    public override void Execute()
+    {
+        receiver.Operation();
+    }
+    public override void Undo()
+    {
+        throw new NotImplementedException();
+    }
+}
+class Receiver
+{
+    public void Operation() { }
+}
+class Invoker
+{
+    Command command;
+    public void SetCommand(Command c) 
+    {
+        command = c;
+    }
+    public void Run() 
+    {
+        command.Execute();
+    }
+    public void Cancel() 
+    {
+        command.Undo();
+    }
+}
+class Client
+{
+    void Main()
+    {
+        Invoker invoker=new Invoker();
+        Receiver receiver=new Receiver();
+        ConcreteCommand concreteCommand = new ConcreteCommand(receiver);
+        invoker.SetCommand(concreteCommand);
+        invoker.Run();
+    }
+}
---
+++ IteratorProject/Program.cs
@@ -0,0 +1,155 @@
+﻿//using System.Collections;
+
+//abstract class Iterator
+//{
+//    public abstract object First();
+//    public abstract object Next();
+//    public abstract bool IsDone();
+//    public abstract object Current();
+//}
+//class ConcreteIterator : Iterator
+//{
+//    private readonly Aggregate _aggregate;
+//    public int _current;
+//    public ConcreteIterator(Aggregate aggregate)
+//    {
+//        _aggregate = aggregate;
+//    }
+//    public override object Current()
+//    {
+//        return _aggregate[_current];
+//    }
+//    public override object First()
+//    {
+//        return _aggregate[0];
+//    }
+//    public override bool IsDone()
+//    {
+//        return _current>=_aggregate.Count;
+//    }
+//    public override object Next()
+//    {
+//        object? ret = null;
+//        _current++;
+//        if(_current<_aggregate.Count)
+//        {
+//            ret= _aggregate[_current];
+//        }
+//        return ret!;
+//    }
+//}
+//abstract class Aggregate
+//{
+//    public abstract Iterator CreateIterator();
+//    public abstract int Count { get; protected set; }
+//    public abstract object this[int index] { get; set; }
+//}
+//class ConcreteAggregate : Aggregate
+//{
+//    private readonly ArrayList _item = new ArrayList();
+//    public override object this[int index]
+//    {
+//        get { return _item[index]!; }
+//        set { _item.Insert(index, value); }
+//    }
+//    public override int Count
+//    {
+//        get { return _item.Count; }
+//        protected set { }
+//    }
+//    public override Iterator CreateIterator()
+//    {
+//        return new ConcreteIterator(this);
+//    }
+//}
+
+//class Client
+//{
+//    public void Main()
+//    {
+//        Aggregate a=new ConcreteAggregate();
+//        Iterator i = a.CreateIterator();
+//        object item = i.Next();
+//        while(!i.IsDone())
+//        {
+//            item = i.Next();
+//        }
+//    }
+//}
+
+Library library = new Library();
+Reader reader= new Reader();
+reader.SeeBook(library);
+
+interface IBookIterator
+{
+    bool HasNext();
+    Book Next();
+}
+interface IBookNumerable
+{
+    IBookIterator CreateNumerator();
+    int Count { get; }
+    Book this[int index] { get; }
+}
+class Book
+{
+    public string? Name { get; set; }
+}
+class Reader
+{
+    public void SeeBook(Library library)
+    {
+        IBookIterator iterator = library.CreateNumerator();
+        while (iterator.HasNext())
+        {
+            Book book = iterator.Next();
+            Console.WriteLine(book.Name);
+        }
+    }
+}
+class Library : IBookNumerable
+{
+    private Book[]? books;
+    public Library()
+    {
+        books = new Book[]
+        {
+            new Book{Name="Яйлян любит морковь"},
+            new Book{Name="Яйлян - снежный человек"}
+        };
+    }
+    public Book this[int index]
+    {
+        get
+        {
+            return books![index];
+        }
+    }
+    public int Count { get { return books!.Length; } }
+
+    public IBookIterator CreateNumerator()
+    {
+        return new LibraryNumerator(this);
+    }
+}
+class LibraryNumerator : IBookIterator
+{
+    IBookNumerable? aggregate;
+    int index = 0;
+
+    public LibraryNumerator(IBookNumerable? a)
+    {
+        this.aggregate = a;
+    }
+
+    public bool HasNext()
+    {
+        return index<aggregate!.Count;
+    }
+
+    public Book Next()
+    {
+        return aggregate![index++];
+    }
+}
